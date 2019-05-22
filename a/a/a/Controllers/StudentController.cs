aa59c7f77abf62cf5f51686436a3e5018a24cb7a
0.0 <h24661078@mailst.cjcu.edu.tw>
2019-05-22 15:36:38 +0800

    0.1

--- a/a/a/Controllers/StudentController.cs
+++ a/a/a/Controllers/StudentController.cs
@@ -16,9 +16,37 @@ namespace a.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index()
+        /*public ActionResult Index()
         {
             return View(db.Students.ToList());
+        }*/
+        public ActionResult Index(string sortOrder ,string searchString)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var students = from s in db.Students
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+            return View(students.ToList());
         }
 
         // GET: Student/Details/5
@@ -84,42 +112,76 @@ namespace a.Controllers
         // POST: Student/Edit/5
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(student).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(student);
+        //}
 
-        // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+         //GET: Student/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = db.Students.Find(id);
+        //    if (student == null)
+        //    {
+        //       return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
+{
+    if (id == null)
+    {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    }
+    if (saveChangesError.GetValueOrDefault())
+    {
+        ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+    }
+    Student student = db.Students.Find(id);
+    if (student == null)
+    {
+        return HttpNotFound();
+    }
+    return View(student);
+}
 
         // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
+        /*[HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
             Student student = db.Students.Find(id);
             db.Students.Remove(student);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
@@ -131,5 +193,31 @@ namespace a.Controllers
             }
             base.Dispose(disposing);
         }
+        [HttpPost, ActionName("Edit")]
+[ValidateAntiForgeryToken]
+public ActionResult EditPost(int? id)
+{
+    if (id == null)
+    {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    }
+    var studentToUpdate = db.Students.Find(id);
+    if (TryUpdateModel(studentToUpdate, "",
+       new string[] { "LastName", "FirstMidName", "EnrollmentDate" }))
+    {
+        try
+        {
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        catch (DataException /* dex */)
+        {
+            //Log the error (uncomment dex variable name and add a line here to write a log.
+            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+        }
+    }
+    return View(studentToUpdate);
+}
     }
 }
